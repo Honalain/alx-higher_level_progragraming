aaad4a696b65ac1b021b91eadfccb5ecc6fed321
root <root@Nyirinkwaya>
2024-03-19 19:01:00 +0200

    This is the Data structure :List,Tuples

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
         return my_list[:]
@@ -6,4 +7,3 @@ def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     new_list[idx] = element
     return new_list
-
