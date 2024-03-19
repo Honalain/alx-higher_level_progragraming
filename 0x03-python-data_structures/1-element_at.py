d91eb38e172beea868c7c1b17869ad1fadc40a25
root <root@Nyirinkwaya>
2024-03-19 11:15:29 +0200

    This is the Data structure :List,Tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,15 +1,10 @@
 #!usr/bin/python3
-
 def element_at(my_list, idx):
-
     if idx < 0:
         return None
     count = 0
-
     for elem in my_list:
         count +=count
-
     if idx >= count:
         return None
-
     return my_list[idx]
