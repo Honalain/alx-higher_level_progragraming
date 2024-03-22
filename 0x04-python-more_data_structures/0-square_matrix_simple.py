9cc9151dbe3fb6ad5ddb8e6f49a326361d628758
root <root@Nyirinkwaya>
2024-03-22 21:32:23 +0200

    This is 0x17. C - Doubly linked lists project Done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+
     result_matrix = []
     for row in matrix:
         result_row = []
         for num in row:
             result_row.append(num** 2)
-
-        result_matrix.append(result_row)
-
+    result_matrix.append(result_row)
     return result_matrix
+result = [[num ** 2]for i in row] for row in matrix
+
