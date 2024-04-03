b51339d88348b483ad54ef39101dd123a5e769e5
root <root@Nyirinkwaya>
2024-04-03 22:33:51 +0200

    This task for 0x05. Python - Exceptions Alx SE project done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -9,10 +9,9 @@ def safe_print_integer(value):
     Returns:
         If a TypeError or ValueError occurs - False.
         Otherwise - True.
-        
     """
     try:
         print("{:d}".format(value))
         return (True)
-    except (ValueError,TypeError):
+    except (ValueError, TypeError):
         return (False)
